b86d5ac24997b5b576907a9098f8666c240595af
Mauricio Zanetti Filho <admin@whostler.com>
2023-02-21 13:46:32 -0300

    create strategy white Rabbit

---
+++ hummingbot/strategy/white_rabbit/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+from .white_rabbit import WhiteRabbitStrategy
+from .inventory_cost_price_delegate import InventoryCostPriceDelegate
+__all__ = [
+    WhiteRabbitStrategy,
+    InventoryCostPriceDelegate,
+]
