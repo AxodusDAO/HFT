0915926fb72c8fbc129e82a109c86521c74f5e3d
Mauricio Zanetti Filho <admin@whostler.com>
2023-02-24 21:58:10 -0300

    pdate script pmm

--- scripts/hilo_spot.py
+++ scripts/hilo_spot.py
@@ -1,48 +1,29 @@
-import logging
 from collections import deque
 from decimal import Decimal
 from statistics import mean
+from conf.strategies.spot_white_rabbit import exchange, market 
 
-
-from hummingbot.core.event.events import (
-    BuyOrderCompletedEvent,
-    SellOrderCompletedEvent
-)
 from hummingbot.core.data_type.common import OrderType
-from hummingbot.pmm_script.pmm_script_base import PMMScriptBase
+from hummingbot.strategy.script_strategy_base import ScriptStrategyBase
 
 
-class HiLoSpot(PMMScriptBase):
-    """
-        Demonstrates how to set up a ping pong trading strategy which alternates buy and sell orders.
-        If a buy order is filled, there will be one less buy order submitted at the next refresh cycle.
-        If a sell order is filled, there will be one less sell order submitted at the next refresh cycle.
-        The balance is positive if there are more completed buy orders than sell orders.
-    """
-     
+class buyLowSellHigh(ScriptStrategyBase):
+    connectors = exchange
+    markets = market
     #: pingpong is a variable to allow alternating between buy & sell signals
-    def __init__(self):
-        super().__init__()
-        self.ping_pong_balance = 0
+    pingpong = 0
 
     """
-        for the sake of simplicity in testing, we will define fast MA as the 7-candles-MA, and slow MA as the
-        75-candles-MA. User can change this as desired
+    for the sake of simplicity in testing, we will define fast MA as the 5-secondly-MA, and slow MA as the
+    20-secondly-MA. User can change this as desired
     """
-    de_fast_ma = deque([], maxlen=7)
-    de_slow_ma = deque([], maxlen=75)
 
-    def logger(cls):
-        global mpb_logger
-        if mpb_logger is None:
-            mpb_logger = logging.getLogger(__name__)
-        return mpb_logger
+    de_fast_ma = deque([], maxlen=5)
+    de_slow_ma = deque([], maxlen=20)
 
     def on_tick(self):
-        strategy = self.pmm_parameters
-        buys = strategy.order_levels
-        sells = strategy.order_levels
-        
+        p = self.connectors[exchange].get_price(market, True)
+
         #: with every tick, the new price of the trading_pair will be appended to the deque and MA will be calculated
         self.de_fast_ma.append(p)
         self.de_slow_ma.append(p)
@@ -52,17 +33,23 @@ class HiLoSpot(PMMScriptBase):
         #: logic for golden cross
         if (fast_ma > slow_ma) & (self.pingpong == 0):
             self.buy(
-                order_type=OrderType.LIMIT,
+                connector_name="binance_paper_trade",
+                trading_pair="BTC-USDT",
+                amount=Decimal(0.01),
+                order_type=OrderType.MARKET,
             )
-            self.logger().info(f'{"%s BTC bought"}')
+            self.logger().info(f'{"0.01 BTC bought"}')
             self.pingpong = 1
 
         #: logic for death cross
         elif (slow_ma > fast_ma) & (self.pingpong == 1):
             self.sell(
-                order_type=OrderType.LIMIT,
+                connector_name="binance_paper_trade",
+                trading_pair="BTC-USDT",
+                amount=Decimal(0.01),
+                order_type=OrderType.MARKET,
             )
-            self.logger().info(f'{"%s BTC sold"}')
+            self.logger().info(f'{"0.01 BTC sold"}')
             self.pingpong = 0
 
         else:
