49918c36b1a2946b4968430f72bbd095401513b2
Mauricio Zanetti Filho <admin@whostler.com>
2023-02-27 23:05:40 -0300

    add MA_ and rsi_ call

--- hummingbot/strategy/white_rabbit/start.py
+++ hummingbot/strategy/white_rabbit/start.py
@@ -55,6 +55,14 @@ def start(self):
         order_refresh_tolerance_pct = c_map.get("order_refresh_tolerance_pct").value / Decimal('100')
         order_override = c_map.get("order_override").value
         split_order_levels_enabled = c_map.get("split_order_levels_enabled").value
+        ma_cross_enabled = c_map.get("ma_cross_enabled").value
+        fast_ma = c_map.get("fast_ma").value
+        slow_ma = c_map.get("slow_ma").value
+        rsi_enabled = c_map.get("rsi_enabled").value
+        rsi_timeframe = c_map.get("rsi_timeframe").value
+        rsi_length = c_map.get("rsi_length").value
+        rsi_oversold_level = c_map.get("rsi_oversold_level").value
+        rsi_overbought_level = c_map.get("rsi_overbought_level").value
         moving_price_band = MovingPriceBand(
             enabled=c_map.get("moving_price_band_enabled").value,
             price_floor_pct=c_map.get("price_floor_pct").value,
@@ -135,9 +143,18 @@ def start(self):
             hb_app_notification=True,
             order_override={} if order_override is None else order_override,
             split_order_levels_enabled=split_order_levels_enabled,
+            ma_cross_enabled=ma_cross_enabled,
+            fast_ma=fast_ma,
+            slow_ma=slow_ma,
+            rsi_enabled=rsi_enabled,
+            rsi_timeframe=rsi_timeframe,
+            rsi_length=rsi_length,
+            rsi_oversold_level=rsi_oversold_level,
+            rsi_overbought_level=rsi_overbought_level,
             bid_order_level_spreads=bid_order_level_spreads,
             ask_order_level_spreads=ask_order_level_spreads,
             should_wait_order_cancel_confirmation=should_wait_order_cancel_confirmation,
+
             moving_price_band=moving_price_band
         )
     except Exception as e:
