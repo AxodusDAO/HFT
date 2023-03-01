c442323262ae3e6e77d1dc86d794f525cb25b99a
Mauricio Zanetti Filho <admin@whostler.com>
2023-03-01 19:39:39 -0300

    change chase

--- hummingbot/indicators/moving_average.py
+++ hummingbot/indicators/moving_average.py
@@ -3,22 +3,19 @@ from enum import Enum
 from decimal import Decimal
 
 class MovingAverageType(Enum):
-sma = "SMA"
-ema = "EMA"
-wma = "WMA"
+SMA = "SMA"
+EMA = "EMA"
+WMA = "WMA"
 
 class MovingAverageCalculator:
-    def init(self, prices: List[Decimal], period: int, ma_type: MovingAverageType):
-        self.prices = prices
-        self.period = period
-        self.ma_type = ma_type
-
     def get_sma(self) -> Decimal:
         if len(self.prices) < self.period:
             return None
         return sum(self.prices[-self.period:]) / Decimal(self.period)
 
     def get_ema(self) -> Decimal:
+        if len(self.prices) < self.period:
+            return None
         alpha = 2 / (self.period + 1)
         ema = self.prices[-self.period]
         for price in reversed(self.prices[-self.period + 1:]):
@@ -28,16 +25,16 @@ class MovingAverageCalculator:
     def get_wma(self) -> Decimal:
         if len(self.prices) < self.period:
             return None
-        weights = list(range(1, self.period + 1))
+        weights = list(reversed(range(1, self.period + 1)))
         weighted_prices = [w * p for w, p in zip(weights, self.prices[-self.period:])]
         return sum(weighted_prices) / Decimal(sum(weights))
 
     def calculate(self) -> Decimal:
-        if self.ma_type == MovingAverageType.sma:
+        if self.ma_type == MovingAverageType.SMA:
             return self.get_sma()
-        elif self.ma_type == MovingAverageType.ema:
+        elif self.ma_type == MovingAverageType.EMA:
             return self.get_ema()
-        elif self.ma_type == MovingAverageType.wma:
+        elif self.ma_type == MovingAverageType.WMA:
             return self.get_wma()
         else:
             raise ValueError(f"Invalid MovingAverageType: {self.ma_type}")
