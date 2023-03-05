9206b8b70ff0921a96deb8a31781ac13bd9c48ec
Mauricio Zanetti Filho <admin@whostler.com>
2023-03-05 09:44:39 -0300

    def  toggle to enable / disable strategy

--- hummingbot/strategy/white_rabbit/ma_cross.py
+++ hummingbot/strategy/white_rabbit/ma_cross.py
@@ -1,24 +1,28 @@
 from typing import List
 from decimal import Decimal
+from collections import deque
 from hummingbot.core.event.events import TradeType
 from hummingbot.indicators.moving_average import MovingAverageCalculator, MovingAverageType
 
 class MACross:
     enable: bool = False
     def __init__(self, period: int, ma_type: MovingAverageType, fast_ma: int, slow_ma: int):
+        assert isinstance(period, int) and isinstance(fast_ma, int) and isinstance(slow_ma, int), \
+            "period, fast_ma, and slow_ma must be integers"
+        assert period > fast_ma and period > slow_ma, "period must be greater than fast_ma and slow_ma"
         self._ma_calculator = MovingAverageCalculator(period, ma_type)
         self._fast_ma = fast_ma
         self._slow_ma = slow_ma
-        self._buys = []
-        self._sells = []
+        self._buys = deque()
+        self._sells = deque()
 
     @property
     def buys(self) -> List[TradeType]:
-        return self._buys
+        return list(self._buys)
 
     @property
     def sells(self) -> List[TradeType]:
-        return self._sells
+        return list(self._sells)
 
     def update(self, price: Decimal):
         self._ma_calculator.add_new_price(price)
@@ -30,9 +34,9 @@ class MACross:
         elif slow_ma > fast_ma:
             self._sells.append(TradeType.SELL)
     
-    def switch(self, value: bool) -> None:
+    def toggle(self) -> None:
         '''
-        switch between enabled and disabled state
-        :param value: set whether to enable or disable MACross
+        toggle between enabled and disabled state
         '''
-        self.enable = value
+        self.enable = not self.enable
+
