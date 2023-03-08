dc3404e5d17f3e0fdc9838f0a1ae66788422ac97
Mauricio Zanetti Filho <admin@whostler.com>
2023-03-08 06:50:28 -0300

    dev update

--- hummingbot/strategy/white_rabbit/start.py
+++ hummingbot/strategy/white_rabbit/start.py
@@ -56,11 +56,12 @@ def start(self):
         order_refresh_tolerance_pct = c_map.get("order_refresh_tolerance_pct").value / Decimal('100')
         order_override = c_map.get("order_override").value
         split_order_levels_enabled = c_map.get("split_order_levels_enabled").value
-        ma_cross = c_map.get("ma_cross").value
-        ma_type=c_map.get("ma_type").value
-        fast_ma=c_map.get("fast_ma").value
+        ma_cross = MACross( 
+        enable=c_map.get("ma_cross").value,
+        ma_type=c_map.get("ma_type").value,
+        fast_ma=c_map.get("fast_ma").value,
         slow_ma=c_map.get("slow_ma").value
-        
+        )
                 
         moving_price_band = MovingPriceBand(
             enabled=c_map.get("moving_price_band_enabled").value,
