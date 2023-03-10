32cbbe1475314265abf33ad5e197b089331e9ed9
Mauricio Zanetti Filho <admin@whostler.com>
2023-03-09 23:10:44 -0300

    dev update

--- hummingbot/strategy/white_rabbit/ma_cross.py
+++ hummingbot/strategy/white_rabbit/ma_cross.py
@@ -3,53 +3,43 @@ from dataclasses import dataclass
 from typing import List
 from decimal import Decimal
 from hummingbot.core.event.events import TradeType
-from hummingbot.indicators.moving_average import MovingAverageCalculator, MovingAverageType
+from hummingbot.indicators.moving_average import MACalc
 
 
 class MACross:
-    def __init__(self, period: int, fast_ma: int, slow_ma: int, ma_type: MovingAverageType = MovingAverageType.SMA, enabled = False):
-        self.enabled = enabled
-        self.ma_type = ma_type
-        self.period = period
-        self.ma_calculator = MovingAverageCalculator(self._ma_type, self._period)
-        self.fast_ma = fast_ma
-        self.slow_ma = slow_ma
+    enabled: bool = False 
+    ma_type: str = "ema"
+    period: int = 3200
+    fast_ma: int = 50
+    slow_ma: int = 200
+    
+    def __post_init__(self):
+        self.ma_calculator = MACalc(self.ma_type, self.period)
         self.buys = []
         self.sells = []
         self.last_action = None
-
-    def update(self, price: Decimal) -> bool:
+    
+    def update(self, price: Decimal):
         self.ma_calculator.add_new_sample(price)
         self.fast_ma = self.ma_calculator.get_moving_average(self.fast_ma)
         self.slow_ma = self.ma_calculator.get_moving_average(self.slow_ma)
 
-        if self.fast_ma > self.slow_ma and self.last_action != TradeType.BUY:
+        if self.fast_ma > self.slow_ma and self.should_buy():
             self.buys.append(TradeType.BUY)
             self.last_action = TradeType.BUY
             return True
 
-        elif self.slow_ma > self.fast_ma and self.last_action != TradeType.SELL:
+        elif self.slow_ma > self.fast_ma and self.should_sell():
             self.sells.append(TradeType.SELL)
             self.last_action = TradeType.SELL
             return True
 
         return False
 
-    def is_above_slow_ma(self, price: Decimal) -> bool:
-        return price > self.ma_calculator.get_moving_average(self.slow_ma)
+    def should_buy(self) -> bool:
+        return self.last_action != TradeType.BUY
 
-    def is_below_slow_ma(self, price: Decimal) -> bool:
-        return price < self.ma_calculator.get_moving_average(self.slow_ma)
+    def should_sell(self) -> bool:
+        return self.last_action != TradeType.SELL
 
-    def should_buy(self, price: Decimal) -> bool:
-        return self.is_above_slow_ma(price) and self.last_action != TradeType.BUY
 
-    def should_sell(self, price: Decimal) -> bool:
-        return self.is_below_slow_ma(price) and self.last_action != TradeType.SELL
-
-    def switch(self, value: bool) -> None:
-        '''
-        switch between enabled and disabled state
-        :param value: set whether to enable or disable MACross
-        '''
-        self.enabled = value
