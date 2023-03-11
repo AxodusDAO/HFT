b8b83a3f4472dd837c8b8c925fa2f9b646bd7537
Mauricio Zanetti Filho <admin@whostler.com>
2023-03-11 06:03:00 -0300

    dev update

--- hummingbot/strategy/white_rabbit/ma_cross.py
+++ hummingbot/strategy/white_rabbit/ma_cross.py
@@ -5,40 +5,50 @@ from decimal import Decimal
 from hummingbot.core.event.events import TradeType
 from hummingbot.indicators.moving_average import MACalc
 
-class MAType:
-    sma = MACalc.get_sma(prices, n)
-    ema = MACalc.get_ema(prices, n)
-class MACross:
-    enabled: bool = False 
-    ma_type: MAType()
-    period: int = 3200
-    fast_ma: int = 9
-    slow_ma: int = 50
-    
+
+@dataclass
+class MAType(MACalc):
+    prices: List[Decimal]
+    n: int
+    fast_sma: Decimal = None
+    fast_ema: Decimal = None
+    slow_sma: Decimal = None
+    slow_ema: Decimal = None
+
     def __post_init__(self):
+        self.fast_sma = self.get_sma(self.prices, self.fast_ma)
+        self.fast_ema = self.get_ema(self.prices, self.fast_ma)
+        self.slow_sma = self.get_sma(self.prices, self.slow_ma)
+        self.slow_ema = self.get_ema(self.prices, self.slow_ma)
+
+
+class MACross:
+    enabled = False 
+    ma_type: MAType
+    period = 3200
+    fast_ma = 9
+    slow_ma = 50
+    buys = []
+    sells = []
+    last_action = None
+
+    def __init__(self, prices: List[Decimal], n: int):
+        self.ma_type = MAType(prices, n)
         self.ma_calculator = MACalc()
-        self.buys = []
-        self.sells = []
-        self.last_action = None
-    
+
     def update(self, price: Decimal):
-        logging.debug(f"Price: {price}")
         self.ma_calculator.add_new_sample(price)
-        self.fast_ma = self.ma_calculator.get_moving_average(self.fast_ma)
-        self.slow_ma = self.ma_calculator.get_moving_average(self.slow_ma)
-        logging.debug(f"Fast MA: {self.fast_ma}")
-        logging.debug(f"Slow MA: {self.slow_ma}")
+        self.fast_ma = self.ma_calculator.get_moving_average(self.ma_type.fast_ema if self.ma_type.ma_type == "ema" else self.ma_type.fast_sma)
+        self.slow_ma = self.ma_calculator.get_moving_average(self.ma_type.slow_ema if self.ma_type.ma_type == "ema" else self.ma_type.slow_sma)
 
         if self.fast_ma > self.slow_ma and self.should_buy():
             self.buys.append(TradeType.BUY)
             self.last_action = TradeType.BUY
-            logging.debug("Buy signal detected")
             return True
 
         elif self.slow_ma > self.fast_ma and self.should_sell():
             self.sells.append(TradeType.SELL)
             self.last_action = TradeType.SELL
-            logging.debug("Sell signal detected")
             return True
 
         return False
