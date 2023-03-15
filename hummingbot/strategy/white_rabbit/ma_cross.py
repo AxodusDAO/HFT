ee7b6990eac6a08c14933e49f4786cfd6a987282
Mauricio Zanetti Filho <admin@whostler.com>
2023-03-15 08:56:01 -0300

    dev update

--- hummingbot/strategy/white_rabbit/ma_cross.py
+++ hummingbot/strategy/white_rabbit/ma_cross.py
@@ -2,34 +2,26 @@ import logging
 from dataclasses import dataclass
 from decimal import Decimal
 from hummingbot.core.event.events.trade_type import TradeType
-from hummingbot.indicators.moving_average import MACalc, MAType
+from hummingbot.indicators.moving_average import MACalc
 
 
-@dataclass
 class MACross:
-    enabled: bool = False
-    ma_type: str = "sma"
-    timestamp: float = None
-    fast_ma: float = None
-    slow_ma: float = None
-    prices: list = []
-    buys: list = []
-    sells: list = []
-    last_action: TradeType = None
-
-    def __post_init__(self):
-        self.ma_type = MAType(self.ma_type, self.prices, self.timestamp)
-        self.fast_ma = self.ma_type.get_ma()
-        self.slow_ma = self.ma_type.get_ma() * 2
-
-    @classmethod
-    def logger(cls):
-        return logging.getLogger(__name__)
+    def __init__(self, ma_type: str = "sma"):
+        self.enabled = False
+        self.ma_type = ma_type
+        self.timestamp = None
+        self.fast_ma = None
+        self.slow_ma = None
+        self.prices = []
+        self.buys = []
+        self.sells = []
+        self.last_action = None
+        self.logger = logging.getLogger(__name__)
 
     def get_ma(self):
-        if self.ma_type.ma_type == "sma":
+        if self.ma_type == "sma":
             return MACalc.get_sma(self.prices, self.timestamp)
-        elif self.ma_type.ma_type == "ema":
+        elif self.ma_type == "ema":
             return MACalc.get_ema(self.prices, self.timestamp)
 
     def golden_cross(self) -> bool:
@@ -54,8 +46,8 @@ class MACross:
 
     def update(self, timestamp: float, price: Decimal) -> bool:
         self.prices.append(price)
-        self.fast_ma = self.ma_type.get_ma()
-        self.slow_ma = self.ma_type.get_ma() * 2
+        self.fast_ma = self.get_ma()
+        self.slow_ma = self.get_ma() * 2
         return self.golden_cross() or self.death_cross()
 
     def switch(self, value: bool) -> None:
