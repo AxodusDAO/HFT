4d040b80b5737e9c6f619ed8c2ba8f1f23ff0006
Mauricio Zanetti Filho <admin@whostler.com>
2023-03-18 12:06:30 -0300

    dev update

--- hummingbot/strategy/white_rabbit/ma_cross.py
+++ hummingbot/strategy/white_rabbit/ma_cross.py
@@ -1,4 +1,6 @@
+import pandas  as pd
 import dataclasses
+
 from dataclasses import dataclass
 from typing import List
 from decimal import Decimal
@@ -16,32 +18,35 @@ class MACross:
     last_action: str = None # Add last_action attribute to track the last action taken
     buys: List[str] = dataclasses.field(default_factory=list) # Add buys attribute to store buy actions
     sells: List[str] = dataclasses.field(default_factory=list) # Add sells attribute to store sell actions
-
+    _ma_type: str = "sma"
 
     # Method to calculate moving average based on the type and period
     def get_ma(self, tf: int) -> List[Decimal]:
-        return self.tf.get_ma(self.prices, tf, self.ma_type)
+        if self.ma_type == "sma":
+            return self.tf.get_sma(self.prices, tf)
+        elif self.ma_type == "ema":
+            return self.tf.get_ema(self.prices, tf)
 
     @property
     def ma_type(self) -> str:
-        if self._ma_type == "sma":
-            return "SMA"
-        elif self._ma_type == "ema":
-            return "EMA"
         return self._ma_type
+
+    @ma_type.setter
+    def ma_type(self, value: str): 
+        self._ma_type = value
 
 
     # Method to determine if a golden cross occurred (fast_ma > slow_ma) and should buy
-    def golden_cross(self) -> bool:
-        if self.fast_ma > self.slow_ma and self.should_buy():
+    def golden_cross(self, fast_ma_value: Decimal, slow_ma_value: Decimal) -> bool:
+        if fast_ma_value > slow_ma_value and self.should_buy():
             self.buys.append("BUY")
             self.last_action = "BUY"
             return True
         return False
 
     # Method to determine if a death cross occurred (slow_ma > fast_ma) and should sell
-    def death_cross(self) -> bool:
-        if self.slow_ma > self.fast_ma and self.should_sell():
+    def death_cross(self, fast_ma_value: Decimal, slow_ma_value: Decimal) -> bool:
+        if slow_ma_value > fast_ma_value and self.should_sell():
             self.sells.append("SELL")
             self.last_action = "SELL"
             return True
@@ -59,10 +64,11 @@ class MACross:
     def update(self, price: Decimal) -> bool:
         self.prices.append(price)
         if len(self.prices) >= self.slow_ma:
-            self.fast_ma = self.get_ma(self.fast_ma)[-1]
-            self.slow_ma = self.get_ma(self.slow_ma)[-1]
-            return self.golden_cross() or self.death_cross()
+            fast_ma_value = self.get_ma(self.fast_ma)[-1]
+            slow_ma_value = self.get_ma(self.slow_ma)[-1]
+            return self.golden_cross(fast_ma_value, slow_ma_value) or self.death_cross(fast_ma_value, slow_ma_value)
         return False
+
 
     # Method to enable or disable the moving average cross strategy
     def switch(self, value: bool) -> None:
