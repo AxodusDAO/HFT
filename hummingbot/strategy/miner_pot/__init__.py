57d4a4e4fb184b9054899d8e0247f8a50b4f76b9
Mauricio Zanetti Filho <admin@whostler.com>
2023-03-28 07:44:27 -0300

    update strategy Miner POT

---
+++ hummingbot/strategy/miner_pot/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+from .miner_pot import MinerPOTStrategy
+from .inventory_cost_price_delegate import InventoryCostPriceDelegate
+__all__ = [
+    MinerPOTStrategy,
+    InventoryCostPriceDelegate,
+]
