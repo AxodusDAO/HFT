450b2140e8519ce9ccbddcfafe0a90615c6bf113
Mauricio Zanetti Filho <admin@whostler.com>
2023-04-16 20:21:43 -0300

    update

--- hummingbot/strategy/__utils__/trailing_indicators/insta_volume.py
+++ hummingbot/strategy/__utils__/trailing_indicators/insta_volume.py
@@ -3,15 +3,15 @@ import numpy as np
 
 
 class VolumeIndicator(BaseTrailingIndicator):
-    def __init__(self, sampling_length: int = 10, processing_length: int = 1):
+    def __init__(self, sampling_length: int = 10, processing_length: int = 5):
         super().__init__(sampling_length, processing_length)
 
     def _indicator_calculation(self) -> float:
         # Instead of calculating volatility, we will calculate the sum of volume
         # in the sampling buffer as the instant volume.
         np_sampling_buffer = self._sampling_buffer.get_as_numpy_array()
-        instant_volume = np.sum(np_sampling_buffer)
-        return instant_volume
+        volume = np.sum(np_sampling_buffer)
+        return volume
 
     def _processing_calculation(self) -> float:
         # Only the last calculated volume, not an average of multiple past volumes
