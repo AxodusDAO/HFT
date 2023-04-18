f55a6c1edaa10e7afa72520d12fdfe407b92f524
Mauricio Zanetti Filho <admin@whostler.com>
2023-04-18 18:40:59 -0300

    dev

--- hummingbot/strategy/__utils__/trailing_indicators/vma.py
+++ hummingbot/strategy/__utils__/trailing_indicators/vma.py
@@ -1,21 +1,19 @@
-from .base_trailing_indicator import BaseTrailingIndicator
 import pandas as pd
+from .base_trailing_indicator import BaseTrailingIndicator
 
-class VolumeIndicator(BaseTrailingIndicator):
-    def __init__(self, sampling_length: int = 1, processing_length: int = 1):
-        if sampling_length != 1:
-            raise Exception("InstantVolumeIndicator sampling_length should be 1")
+class VolumeAverageIndicator(BaseTrailingIndicator):
+    def __init__(self, sampling_length: int = 30, processing_length: int = 15):
         super().__init__(sampling_length, processing_length)
 
     def _indicator_calculation(self) -> float:
-        volume_data = pd.DataFrame(self._sampling_buffer.get_as_numpy_array(),
-                                   columns=['volume'])
-        trading_vol = volume_data['volume'].iloc[-1]
-
-        return trading_vol
+        volumes = self._sampling_buffer.get_as_pandas_series()
+        if not volumes.empty:
+            return volumes.mean()
 
     def _processing_calculation(self) -> float:
-        return self._processing_buffer.get_last_value()
+        processing_series = self._processing_buffer.get_as_pandas_series()
+        if not processing_series.empty:
+            return processing_series.sum() / processing_series.size
 
 
 
